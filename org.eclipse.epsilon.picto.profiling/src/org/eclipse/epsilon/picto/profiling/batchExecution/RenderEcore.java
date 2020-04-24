6609e4ec8d728c14d6192f5765a880e091d12801
alfonsodelavega <alfonso.delavega@tuta.io>
2020-04-24 18:47:18 +0100

    Add some initial, not measured executions to avoid strange times
    The first rendering executions return higher times (at least when I
    execute them in my laptop atm). To avoid these abnormal times, this
    commit adds some extra initial executions on top of the configured
    number of repetitions. These extra executions are not measured.

--- org.eclipse.epsilon.picto.profiling/src/org/eclipse/epsilon/picto/profiling/batchExecution/RenderEcore.java
+++ org.eclipse.epsilon.picto.profiling/src/org/eclipse/epsilon/picto/profiling/batchExecution/RenderEcore.java
@@ -17,13 +17,21 @@ public class RenderEcore {
 		profilingStream.println("Model,BatchTimeMillis");
 
 		int numReps = 15;
-		for (int i = 0; i < numReps; i++) {
+		// add some initial executions that won't be measured
+		// avoids higher initial times due to low states of the cpu
+		int notMeasuredExecutions = 3;
+		numReps += notMeasuredExecutions;
+		for (int rep = 0; rep < numReps; rep++) {
 			// render every model in the list
 			for (String modelName : models) {
 				long start = System.currentTimeMillis();
 				ModelRenderer.render(modelsLocation, transformationFile, modelName, metamodel);
 				long end = System.currentTimeMillis();
-				profilingStream.println(String.format("%s,%d", modelName, end - start));
+				if (rep >= notMeasuredExecutions) {
+					System.out.println(
+							String.format("Rep %d: %s", rep - notMeasuredExecutions + 1, modelName));
+					profilingStream.println(String.format("%s,%d", modelName, end - start));
+				}
 			}
 			// then, delete the output directory for a fresh next generation
 			FileUtils.deleteDirectory(new File("gen"));
