c1973ef51a4748b28837cfc10e5f3fdecf16aed9
alfonsodelavega <alfonso.delavega@tuta.io>
2020-05-22 20:11:16 +0100

    Update the genComps standalone transformation

--- org.eclipse.epsilon.picto.profiling/src/org/eclipse/epsilon/picto/profiling/batchExecution/RenderGenComps.java
+++ org.eclipse.epsilon.picto.profiling/src/org/eclipse/epsilon/picto/profiling/batchExecution/RenderGenComps.java
@@ -21,10 +21,12 @@ public class RenderGenComps {
 	public static void runTransformation(boolean parallelExecution) throws Exception {
 
 		String modelsLocation = "models/gencomps/%s";
-		String[] models = {"gencomps-00.4K.model",
-				"gencomps-01.5K.model", "gencomps-03.9K.model",
-				"gencomps-05.8K.model", "gencomps-12.9K.model",
-				"gencomps-29K.model"};
+		String[] models = {
+				"gencomps-03.9K.model",
+				"gencomps-05.8K.model",
+				"gencomps-12.9K.model",
+				"gencomps-29K.model"
+		};
 		String metamodel = "models/ecore/comps.ecore";
 		String transformationFile = "comps2vis-standalone/comps.egx";
 
@@ -36,7 +38,7 @@ public class RenderGenComps {
 		PrintStream profilingStream = new PrintStream(new File(outputFile));
 		profilingStream.println("Model,BatchTimeMillis");
 
-		int numReps = 5;
+		int numReps = 10;
 		// add some initial executions that won't be measured
 		// avoids higher initial times due to low states of the cpu
 		int notMeasuredExecutions = 3;
@@ -49,7 +51,11 @@ public class RenderGenComps {
 				long end = System.currentTimeMillis();
 				if (rep >= notMeasuredExecutions) {
 					System.out.println(
-							String.format("Rep %d: %s", rep - notMeasuredExecutions + 1, modelName));
+							String.format("Rep %d: %s, parallel: %b, time: %d ms",
+									rep - notMeasuredExecutions + 1,
+									modelName,
+									parallelExecution,
+									end - start));
 					profilingStream.println(String.format("%s,%d", modelName, end - start));
 				}
 			}
